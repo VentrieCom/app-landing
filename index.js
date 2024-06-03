8cf60be4196b06b673b1cb8d5c9ebbd2b0cc0dc2
alimohsin-bp <ali.mohsin@bitspro.com>
2024-06-03 18:20:20 +0500

    allowed domains specified

--- index.js
+++ index.js
@@ -1,19 +1,55 @@
-const domain = document.getElementById("domain");
-const form = document.getElementById("form");
-const defaultDomain = localStorage.getItem("domain");
+document.addEventListener("DOMContentLoaded", () => {
+  const domainInput = document.getElementById("domain");
+  const form = document.getElementById("form");
+  const errorMessage = document.getElementById("error-message");
+  const defaultDomain = localStorage.getItem("domain");
+  const allowedDomains = ["ldenim", "arautos", "bmp"];
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  // window.open(domain.value);
-  localStorage.setItem("domain", domain.value);
-  window.location.href = domain.value;
+  const isValidDomain = (domain) => {
+    const domainName = domain.replace(/^https?:\/\//, "").split(".")[0];
+    return allowedDomains.includes(domainName);
+  };
+
+  const formatDomain = (domain) => {
+    let formattedDomain = domain;
+    if (
+      allowedDomains.some((allowedDomain) => domain.startsWith(allowedDomain))
+    ) {
+      if (!domain.includes(".")) {
+        formattedDomain += ".munshee.app";
+      } else if (!domain.endsWith(".app")) {
+        formattedDomain += ".app";
+      }
+      if (!formattedDomain.startsWith("https://")) {
+        formattedDomain = "https://" + formattedDomain;
+      }
+    }
+    return formattedDomain;
+  };
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let domainValue = domainInput.value;
+    if (isValidDomain(domainValue)) {
+      domainValue = formatDomain(domainValue);
+      localStorage.setItem("domain", domainValue);
+      window.location.href = domainValue;
+    } else {
+      errorMessage.textContent = "The domain is not allowed.";
+    }
+  });
+
+  const getDomain = () => {
+    console.log("outside if block in get domain");
+    console.log("defaultDomain:", defaultDomain);
+    console.log("isValidDomain(defaultDomain):", isValidDomain(defaultDomain));
+    if (defaultDomain && isValidDomain(defaultDomain)) {
+      console.log("Inside if block in get domain");
+      const formattedDomain = formatDomain(defaultDomain);
+      domainInput.value = formattedDomain;
+      window.location.href = formattedDomain;
+    }
+  };
+
+  getDomain();
 });
-
-const getDomain = () => {
-  if (defaultDomain) {
-    domain.value = localStorage.getItem("domain");
-    window.location.href = domain.value;
-  }
-};
-
-getDomain();
